712560d2b77c88876ab2724e110c70981af1a2bb
HAIDANG\irvin <maihaidang2222@gmail.com>
2023-11-01 11:03:49 +0700

    add tour

--- src/components/QuotationForm/QuotationForm.jsx
+++ src/components/QuotationForm/QuotationForm.jsx
@@ -8,6 +8,7 @@ import { fetchTrademarks } from '../../utils/apiService';
 import html2pdf from 'html2pdf.js';
 import PromotionTable from '../Promotions/Promotions';
 import html2canvas from 'html2canvas';
+import Joyride from 'react-joyride';
 
 const QuotationForm = ({ onFormValidationChange }) => {
   const [selectedSupplier, setSelectedSupplier] = useState('');
@@ -26,6 +27,54 @@ const QuotationForm = ({ onFormValidationChange }) => {
     console.log("OOOOOOOOOOO",isValid);
     onFormValidationChange(isValid);
   }
+
+  const [isTourOpen, setIsTourOpen] = useState(false);
+
+  const steps = [
+    {
+      target: '.dropdown-button', 
+      content: 'Bước 1: Chọn nhà cung cấp.',
+      disableBeacon: true,
+    },
+    {
+      target: '.table-cell.product', 
+      content: 'Bước 2: Chọn sản phẩm.',
+    },
+    {
+      target: '.table-cell.description', 
+      content: 'Bước 3: Chọn vật liệu cho sản phẩm.',
+    },
+    {
+      target: '.table-cell.size-item', 
+      content: 'Bước 4: Điền kích thước vào các ô chưa có giá trị sẵn.',
+    },
+    // {
+    //   target: '.table-cell size-item weight', 
+    //   content: 'Bước 5: Nếu sản phẩm có đơn vị là "Cái" hãy nhập số lượng vào ô này.',
+    // },
+    {
+      target: '.add-row-button', 
+      content: 'Bước 5: Click vào đây để thêm hàng mới.',
+    },
+    {
+      target: '.no-print', 
+      content: 'Bước 6: Click vào đây để xuất file pdf lưu lại bảng báo giá.',
+    },
+  ];
+
+  const startTour = () => {
+    setIsTourOpen(true);
+  };
+
+  const closeTour = () => {
+    setIsTourOpen(false);
+  };
+
+  useEffect(() => {
+    startTour(); // Mở hướng dẫn khi component được render
+
+    // Đảm bảo gọi startTour() sau khi dữ liệu suppliers đã được tải từ API
+  }, []);
 
   useEffect(() => {
     async function fetchData() {
@@ -163,6 +212,32 @@ const QuotationForm = ({ onFormValidationChange }) => {
         <a href="https://zalo.me/0703937521" target="_blank" rel="noopener noreferrer"> https://zalo.me/0703937521</a> để được tư vấn đo đạc chính xác nhất.
       </p>
     </div>
+    <Joyride
+        steps={steps}
+        run={isTourOpen}
+        continuous={true}
+        showProgress={true}
+        showSkipButton={true}
+        disableOverlay={false}
+        callback={({ index, status }) => {
+          if (status === 'finished') {
+            // Hướng dẫn đã hoàn thành
+            setIsTourOpen(false);
+          }
+        }}
+        styles={{
+          options: {
+            arrowColor: "#3498db",
+            backgroundColor: "#3498db",
+            overlayColor: "rgba(52, 152, 219, .3)",
+            primaryColor: "#3498db",
+            textColor: "#fff",
+          },
+          spotlight: {
+            backgroundColor: "transparent",
+          },
+        }}
+      />
     </div>
     
   );
